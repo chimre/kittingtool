492d819836dbd550908b14a4c99dab7ba76b3996
chimre <github@reev.jp>
2021-05-24 00:19:13 +0900

    add tensorflow addons

--- admin-03-initialize.ps1
+++ admin-03-initialize.ps1
@@ -6,6 +6,7 @@ pip install --upgrade pycuda
 pip install --upgrade scikit-learn
 pip install --upgrade lightgbm --install-option=--gpu --install-option="--opencl-include-dir=$env:CUDA_HOME\include" --install-option="--opencl-library=$env:CUDA_HOME\lib\x64\OpenCL.lib" --install-option="--boost-root=$env:BOOST_HOME" --install-option="--boost-librarydir=$env:BOOST_HOME\lib64-msvc-14.2"
 pip install --upgrade tensorflow
+pip install --upgrade tensorflow-addons
 pip install --upgrade pandas
 pip install --upgrade protobuf
 pip install --upgrade pelican
