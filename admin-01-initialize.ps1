9327940a083da66406f359742355d7f7ec9f0e97
chimre <github@reev.jp>
2021-09-18 11:13:09 +0900

    add upgrade chocolatey command

--- admin-01-initialize.ps1
+++ admin-01-initialize.ps1
@@ -1,6 +1,9 @@
 $env:ChocolateyInstall = Convert-Path "$((Get-Command choco).path)\..\.."
 Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 
+choco upgrade chocolatey --install-if-not-installed --failonstderr -y
+refreshenv
+
 choco upgrade microsoft-windows-terminal --install-if-not-installed --failonstderr -y
 refreshenv
 
